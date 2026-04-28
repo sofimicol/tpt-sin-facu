b21c787bbc9535b5ffdf36bca338b0b6491b2b05
lautaroP12 <penalozatravainilautaro@gmail.com>
2026-04-28 15:57:18 -0300

    Update TAD_list.h

--- TAD_list.h
+++ TAD_list.h
@@ -1,13 +1,8 @@
 #ifndef TAD_LIST_H
 #define TAD_LIST_H
-
-#include "TAD_AST.h"
-typedef struct dataType* Tdata;
-
+#include "TAD_AST.H"
 void append(Tdata* list, Tdata elem);
 int length(Tdata list);
-Tdata copy_list(Tdata list);
 Tdata concatena_list(Tdata list1, Tdata list2);
 int search(Tdata list, Tdata elem);
-
 #endif
