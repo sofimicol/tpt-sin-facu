9eecafcbe43fd89324e2822e364e2c176a960f05
sofimicol <sofimicol3@gmail.com>
2026-04-28 16:14:05 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,8 @@
 #include "TAD_AST.h"
-
-int main() {
+int main(){
 	
-	// Crear lista
 	Tdata lista = create_list();
 	
-	// Crear strings
 	Tdata s1 = create_str();
 	s1->string = load2("hola");
 	
@@ -15,14 +12,23 @@ int main() {
 	Tdata s3 = create_str();
 	s3->string = load2("test");
 	
-	// Insertar en lista
 	append(&lista, s1);
 	append(&lista, s2);
 	append(&lista, s3);
+	Tdata lista2= create_list();
+	Tdata s6 = create_str();
+	s6->string = load2("hola");
 	
-	// Mostrar resultado
+	Tdata s4 = create_str();
+	s4->string = load2("mundo");
+	
+	Tdata s5 = create_str();
+	s5->string = load2("test");
+	append(&lista2, s4);
+	append(&lista2, s5);
+	append(&lista2, s6);
+	append(&lista,lista2);
 	mostrarArbol(lista);
-	
 	printf("\n");
 	
 	return 0;
