512e3126d3465d6ca3e61d25dc1c454d56ba5166
sofimicol <sofimicol3@gmail.com>
2026-04-28 16:19:29 -0300

    Remove append function declaration from TAD_list.h

--- TAD_list.h
+++ TAD_list.h
@@ -1,7 +1,7 @@
 #ifndef TAD_LIST_H
 #define TAD_LIST_H
 #include "TAD_AST.H"
-void append(Tdata* list, Tdata elem);
+
 int length(Tdata list);
 Tdata concatena_list(Tdata list1, Tdata list2);
 int search(Tdata list, Tdata elem);
