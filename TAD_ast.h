969ad8be10fc677d2774015b467f2a9596553165
lautaroP12 <penalozatravainilautaro@gmail.com>
2026-04-28 15:58:51 -0300

    Update TAD_ast.h

--- TAD_ast.h
+++ TAD_ast.h
@@ -1,13 +1,11 @@
 #ifndef TAD_AST_H
 #define TAD_AST_H
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "TAD_str.h"
+#include "TAD_STR.h"
 #define STR 1
 #define SET 2
 #define LIST 3
-
 
 struct dataType{
 	int nodeType;   /* 1 STR, 2 SET, 3 LIST */
@@ -22,10 +20,16 @@ struct dataType{
 typedef struct dataType* Tdata; // Tdata es un Puntero a un arbol
 Tdata create_str();
 Tdata create_set();
-Tdata prod_cartesiano(Tdata a, Tdata b);
+Tdata prod_cartesiano(Tdata, Tdata);
 Tdata cargar_conjunto();
 Tdata carga_lista();
 Tdata create_list();
-void mostrarArbol(Tdata arbol);
-Tdata prod_cartesiano(Tdata a, Tdata b);
+void mostrarArbol(Tdata);
+Tdata copy_ast(Tdata);
+Tdata create_gen(int);
+int esvacio(Tdata);
+Tdata create_ast();
+int equals_node(Tdata, Tdata);
+Tdata copy_ast(Tdata list);
+void eliminarprimero(Tdata* set);
 #endif
