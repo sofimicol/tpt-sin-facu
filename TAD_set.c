b0c829b2f05044bbd6c8b4334e12bff4e9b4d33b
facu <150148503+chumele@users.noreply.github.com>
2026-04-29 20:56:41 -0300

    Refactor set operations for clarity and efficiency

--- TAD_set.c
+++ TAD_set.c
@@ -15,111 +15,129 @@ void insert_set(Tdata* set, Tdata elem){ //insertar un elemento en un conjunto s
 		}
 	}
 }
-	int belongs(Tdata set, Tdata elem){
-		Tdata aux = set;
-		while(aux != NULL){
-			if(equals_node(aux->data, elem)){
-				return 1;
+int belongs(Tdata set, Tdata elem){ // devuelve 1 si cuando pertenece al conjunto
+	Tdata aux = set;
+	while(aux != NULL && equals_node(aux->data, elem) == 0){
+		aux = aux->next;
+	}
+	if(aux == NULL) {
+		return 0;
+	}else {
+		return 1;
+	}
+}
+void remove_set(Tdata* set, Tdata elem){ //elem es str o set-list
+	Tdata actual;
+	Tdata anterior = NULL;
+	if(esvacio(*set)==1){
+		printf("\nEl conjunto es vacio, no se pudo eliminar elemento");
+	}else{
+		actual=*set;
+		while(actual->next!=NULL && !equals_node(actual->data,elem)){ // funcion generica
+			anterior=actual;
+			actual=actual->next;
+		}
+		if(actual->data==elem->data){ //elemento a eliminar encontrado, se debe eliminar
+			if(anterior==NULL){
+				eliminarprimero(set);
 			}
+			else{
+				anterior->next=actual->next;
+				free(actual);
+				actual=NULL;
+			}
+		}else{
+			printf("\nElemento no encontrado");
+		}
+	}
+}
+Tdata unionset(Tdata A, Tdata B) { 
+	Tdata resultado = create_set();
+	Tdata aux;
+	
+	if(esvacio(A) == 1 && esvacio(B) == 1) {
+		printf("\nConjuntos vacios ");
+		return NULL;
+	}
+	
+	if(esvacio(A) == 1 && esvacio(B) == 0) { // Si A es vacio y B no, devuelvo B
+		aux = B;
+		while(aux != NULL){
+			insert_set(&resultado, copy_ast(aux->data));
 			aux = aux->next;
 		}
-		return 0;
 	}
-	void remove_set(Tdata* set, Tdata elem){ //elem es str o set-list
-		Tdata actual;
-		Tdata anterior=NULL;
-		if(esvacio(*set)==1){
-			printf("\nEl conjunto es vacio, no se pudo eliminar elemento");
-		}else{
-			actual=*set;
-			while(actual->next!=NULL && !equals_node(actual->data,elem)){ // funcion generica
-				anterior=actual;
-				actual=actual->next;
-			}
-			if(actual->data==elem->data){ //elemento a eliminar encontrado, se debe eliminar
-				if(anterior==NULL){
-					eliminarprimero(set);
-				}
-				else{
-					anterior->next=actual->next;
-					free(actual);
-					actual=NULL;
-				}
-			}else{
-				printf("\nElemento no encontrado");
-			}
+	
+	if(esvacio(B) == 1 && esvacio(A) == 0) { // Si B es vacio y A no, devuelvo A
+		aux = A;
+		while(aux != NULL){
+			insert_set(&resultado, copy_ast(aux->data));
+			aux = aux->next;
 		}
+	}
+	
+	aux = A;
+	while(aux != NULL){
+		insert_set(&resultado, copy_ast(aux->data));
+		aux = aux->next;
+	}
+	
+	// Agregar B sin duplicados
+	aux = B;
+	while(aux != NULL){
+		if(belongs(resultado, aux->data) == 0){
+			insert_set(&resultado, copy_ast(aux->data));
+		}
+		aux = aux->next;
+	}
+	
+	return resultado;
 }
-		Tdata unionset(Tdata A, Tdata B) { 
-			Tdata resultado = NULL;
-
-			// copiar A completo
-			Tdata aux = A;
-			while (aux != NULL) {
-				insert_set(&resultado, copy_ast(aux->data));
-				aux = aux->next;
-			}
-			// agregar elementos de B que no estén en A {0,1} {0,1,3}
-			aux = B;
-			while (aux != NULL) {
-				if (belongs(resultado, aux->data)==0) {
-					insert_set(&resultado, copy_ast(aux->data));
-				}
-				aux = aux->next;
-			}
-			return resultado;
+Tdata intersection_set(Tdata A, Tdata B) {
+	Tdata resultado = create_set();
+	Tdata aux;
+	
+	if(esvacio(A) == 1 || esvacio(B) == 1) { // Si A o B no tienen elementos
+		printf("\nInterseccion vacia, NO se puede reslizar la interseccion");
+		return NULL;
+	}
+	// Si A y B tiene elementos aplico porp. de interseccion
+	aux = A;
+	while(aux != NULL) {
+		if(belongs(B, aux->data) == 1) {
+			insert_set(&resultado, copy_ast(aux->data));
 		}
-		Tdata intersection_set(Tdata A, Tdata B) {
-			Tdata resultado = create_set();
-			Tdata aux1;
-			Tdata aux2;
-			Tdata nuevo=NULL;
-			
-			if (esvacio(A)==0) {
-				resultado = copy_ast(A);
-			}else {
-				if(esvacio(B) == 0) {
-					resultado = copy_ast(B);
-				}else {
-					resultado = copy_ast(A);
-					aux1 = A;
-					while(aux1 != NULL) {
-						aux2 = B;
-							while(aux2 != NULL && belongs(aux1->data, aux2->data) == 0) {
-							aux2 = aux2->next;
-						}
-							nuevo = create_set();
-							nuevo->data = copy_ast(aux2->data);
-							append(&resultado, nuevo);
-							aux1 = aux1->next;
-					}
-				}
-			}
-			return resultado;
+		aux = aux->next;
+	}
+	return resultado;
+}
+Tdata difference_set(Tdata A, Tdata B) { // Tomando el encuenta orede de los parametros de tipo A - B
+	Tdata resultado = create_set();
+	Tdata aux = A;
+	
+	if(esvacio(A) == 1) {
+		printf("\nConjunto vacio, NO se puede reslizar la diferencia");
+		return NULL;// Si A y B tiene elementos aplico porp. de diferencia
+	}
+	
+	if(esvacio(B) == 1) {
+		while(aux != NULL){
+			insert_set(&resultado, copy_ast(aux->data));
+			aux = aux->next;
 		}
-		Tdata difference_set(Tdata A, Tdata B) { // Tomando el encuenta orede de los parametros de tipo A - B
-			Tdata resultado=create_set();
-			Tdata aux1;
-			Tdata aux2;
-			Tdata nuevo=NULL;
-			if(esvacio(A) == 0 || esvacio(B) == 0){
-				resultado = copy_ast(A);
-			}else {
-				resultado = copy_ast(A);
-				aux1 = A;
-				while(aux1 != NULL) {
-					aux2 = B;
-					while(aux2 != NULL && belongs(aux1->data, aux2->data) == 1) {
-						aux2 = aux2->next;
-					}
-					nuevo = create_set();
-					nuevo->data = copy_ast(aux2->data);
-					append(&resultado, nuevo);
-					aux1 = aux1->next;
-				}
-			}
-			return resultado;
-		} // son los elementos que pertenecen al primer conjunto y no pertenecen al segundo conjunto // son los elementos que pertenecen al primer conjunto y no pertenecen al segundo conjunto
+		//inserter_set(&resultado, copy_ast(A));
+		return resultado;
+	}
+	
+	while(aux != NULL) {
+		if(belongs(B, aux->data) == 1) {
+			insert_set(&resultado, copy_ast(aux->data));
+		}
+		aux = aux->next;
+	}
+	return resultado;
+}
+ // son los elementos que pertenecen al primer conjunto y no pertenecen al segundo conjunto // son los elementos que pertenecen al primer conjunto y no pertenecen al segundo conjunto
 int equals_set(Tdata A, Tdata B) {
 	int a_en_b = subset(A, B);
 	int b_en_a = subset(B, A);
